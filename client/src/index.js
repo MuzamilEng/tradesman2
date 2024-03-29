f39ad171f2f4bf6bc141ca834cff242d15ce6151
Muzamil <muzekhan777@gmail.com>
2024-03-29 10:40:29 +0500

    removed auth0 from index

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,6 @@ import { Provider } from 'react-redux';
 import { ChakraProvider } from '@chakra-ui/react'
 import './index.css';
 import App from './App';
-import { Auth0Provider } from '@auth0/auth0-react';
 import { UserProvider } from './app/UserContext/UserContext';
 import { store } from './app/store/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,13 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Auth0Provider
-    domain="dev-3ivz2lavdv3bjy5d.us.auth0.com"
-    clientId="i6Emcp030e1VFxsV1ENnaY6XyOvCznaA"
-    authorizationParams={{
-      redirect_uri: window.location.origin
-    }}
-  >
+   
     <Provider store={store}>
       <BrowserRouter>
         <UserProvider>
@@ -29,6 +22,5 @@ root.render(
         </UserProvider>
       </BrowserRouter>
     </Provider>
-    </Auth0Provider>
   </React.StrictMode>
 );
